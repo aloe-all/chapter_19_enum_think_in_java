f4075269d18fc93549de0b11ad375bf5125207d0
rgchen <rgchen@neolink.com.cn>
2016-10-29 08:38:49 +0800

    提交修改

--- src/com/crg/enumDemo/EnumKeyword.java
+++ src/com/crg/enumDemo/EnumKeyword.java
@@ -15,28 +15,40 @@ public class EnumKeyword {
 	public final static int ORANGE = 3;
 	public final static int TOMATO = 4;
 	
+	/**
+	 *  enum 声明
+	 * @author aloe
+	 *
+	 */
+	public enum Fruits{
+		APPLE, BANANA, PEAR, ORANGE, TOMATO
+	}
 	
 	public static void main(String[] args) {
-		int fruitKind = TOMATO;
-		switch (fruitKind) {
-		case APPLE:
-			System.out.println("I want to eat apple");
-			break;
-		case BANANA:
-			System.out.println("I want to eat banana");
-			break;
-		case PEAR:
-			System.out.println("I want to eat pear");
-			break;
-		case ORANGE:
-			System.out.println("I want to eat orange");
-			break;
-		case TOMATO:
-			System.out.println("I want to eat tomato");
-			break;
-		default:
-			System.out.println("there is not any fruit");
-			break;
-		}
+		// 使用 enum 就类似使用普通的 class,创建一个 enum 类型的引用,并给其赋值
+		Fruits oneFruit = Fruits.APPLE;
+		System.out.println(oneFruit);
+//		int fruitKind = TOMATO;
+//		switch (fruitKind) {
+//		case APPLE:
+//			System.out.println("I want to eat apple");
+//			break;
+//		case BANANA:
+//			System.out.println("I want to eat banana");
+//			break;
+//		case PEAR:
+//			System.out.println("I want to eat pear");
+//			break;
+//		case ORANGE:
+//			System.out.println("I want to eat orange");
+//			break;
+//		case TOMATO:
+//			System.out.println("I want to eat tomato");
+//			break;
+//		default:
+//			System.out.println("there is not any fruit");
+//			break;
+//		}
+		
 	}
 }
